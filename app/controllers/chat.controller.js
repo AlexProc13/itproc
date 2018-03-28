d1dae78f2fa81a8daf0fa4f33385d71065f021e5
alexproc <alexproc1313@gmail.com>
2018-03-28 13:56:28 +0300

    fix bug - first) formed many event listening new mail second)send message use id soccet and user_id

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -1,4 +1,5 @@
-const Message = require('../models/message'),
+const
+    Message = require('../models/message'),
     crypto = require('crypto'),
     mail = require('../lib/handler/handlerMail'),
     nodemailer = require('nodemailer'),
@@ -10,8 +11,10 @@ class Chat {
         this.limitMessage = 20;
         this.messages = [{message: 'Hello', date: 0, direction: 'left'}];
         this.code = 'user-it-proc';
+        this.socket_clients = {};
     }
 
+    //set events
     init(io) {
         //for chat
         io.on('connection', function (socket) {
@@ -20,10 +23,9 @@ class Chat {
             socket.on('start chat', function (msg) {
                 //send for mail
                 //send mess
-                //let message = this.startMess;
                 startMessage[0].user = socket.id;
                 startMessage[0].id = this.generate_id();
-                //console.log(startMessage);
+                this.socket_clients[startMessage[0].id] = socket;
                 this.saveToDB(startMessage, socket);
                 socket.emit('get message', startMessage);
             }.bind(this));
@@ -31,16 +33,8 @@ class Chat {
             //get message=====
             socket.on('send message', function (msg) {
                 this.saveToDB(msg, socket);
-                mail.sendMail(nodemailer, this.standartMessege(msg));
                 //send for mail
-                //console.log(msg[0].id);
-                //socket.emit('get message', [{message: '22', date: 0, direction: 'left', id: msg[0].id}]);
-                notifier(mail.imap())
-                    .on('mail', function (mail) {
-                        //socket.emit('get message', [{message: '22', date: 0, direction: 'left', id: msg[0].id}]);
-                        this.listenMail(mail.text, socket);
-                    }.bind(this))
-                    .start();
+                mail.sendMail(nodemailer, this.standartMessege(msg));
             }.bind(this));
 
 
@@ -48,14 +42,20 @@ class Chat {
                 //get values
                 let massages = Message.find().where({id: msg[0].direction}).sort({date: 'desc'}).limit(this.limitMessage).exec();
                 massages.then(function (message) {
-                    //console.log(message);
+                    this.socket_clients[msg[0].direction] = socket;
+                    //console.log(this.socket_clients);
                     socket.emit('preStart chat', message.reverse());
-                });
+                }.bind(this));
             }.bind(this));
 
-
-
         }.bind(this));
+
+        notifier(mail.imap())
+            .on('mail', function (mail) {
+                //console.log(mail.text);
+                this.listenMail(mail.text);
+            }.bind(this))
+            .start();
 
     }
 
@@ -67,10 +67,6 @@ class Chat {
 
     getUser(socket) {
         return 'user@' + socket.id;
-    }
-
-    checkUser(socket) {
-
     }
 
     generate_id() {
@@ -87,28 +83,23 @@ class Chat {
         };
     }
 
-    listenMail(mail, socket){
+    listenMail(mail){
         let parts = mail.split('@user-it-proc@');
         let mess = parts[0].split('@@')[0];
-        //console.log(mess);
         let userId = parts[1].replace(/[^-a-zA-Z-0-9]/gim,'');
         //check
+        let self = this;
         (async function () {
+            ///console.log();
             let messDb = await Message.find().where({id: userId}).exec();
             if (messDb.length > 1){
                 let message = [{message: mess, date: Date.now(), id: userId, direction: 'left'}];
                 //add to database
                 await Message.insertMany(message);
                 //send user
-                socket.emit('get message', message);
+                self.socket_clients[userId].emit('get message', message);
             }
         })();
-        //console.log(mess);
-        //console.log(userId);
-    }
-
-    parseMsg(){
-
     }
 }
 
